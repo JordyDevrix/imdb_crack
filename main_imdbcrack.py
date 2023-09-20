64d4892ad2a1690e05097ebae05ca5e97f5cfc00
Stefan Haasnoot <s1149022@student.hsleiden.nl>
2023-09-20 12:27:53 +0200

    Begin bestand met main() en add_rating()

---
+++ main_imdbcrack.py
@@ -0,0 +1,18 @@
+def add_rating():
+    user_rating = float(input("Please give us your rating: "))
+
+    average_rating = (average_rating + user_rating)/count
+
+    count += 1
+
+    return average_rating
+
+
+add_rating()
+
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
