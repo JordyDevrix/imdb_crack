e0aa8fbcbae614ad16ecdb219148da18c5ba895e
Stefan Haasnoot <s1149022@student.hsleiden.nl>
2023-09-20 12:42:10 +0200

    Toevoeging keuzemenu en kleine stukjes functies

--- main_imdbcrack.py
+++ main_imdbcrack.py
@@ -1,17 +1,41 @@
-def add_rating():
-    user_rating = float(input("Please give us your rating: "))
+def keuzemenu():
+    print("Welkom bij de Internet Movie Database! Kies uit: 1, 2, 3, 4, 5, 6")
+    print("1. Film opvragen")
+    print("2. Film raten")
+    print("3. Beschrijving opvragen")
+    print("ADMIN - > Wachtwoord verplicht")
+    print("4. Film toevoegen")
+    print("5. Serie toevoegen")
+    print("6. Film/serie verwijderen")
 
-    average_rating = (average_rating + user_rating)/count
-
-    count += 1
-
-    return average_rating
-
-
-add_rating()
+# def add_rating():
+#     user_rating = float(input("Please give us your rating: "))
+#
+#     average_rating = (average_rating + user_rating)/count
+#
+#     count += 1
+#
+#     return average_rating
 
 
 def main():
+
+    keuzemenu()
+
+    i = input("Keuze: ")
+
+    # if i == 1:
+    #
+    # if i == 2:
+    #     add_rating()
+    #
+    # if i == 3:
+    #
+    # if i == 4:
+    #
+    # if i == 5:
+    #
+    # if i == 6:
 
 
 if __name__ == '__main__':
